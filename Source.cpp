91ec336661af7285479315da28cd9066d2e3d210
dev <dev@A21PC12>
2024-09-02 17:57:15 +0300

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,68 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{
+	//cout << "To be, or not to be: that is the question:" << endl;
+	//cout << "Whether 'tis nobler in the mind to suffer" << endl;
+	//cout << "The slings and arrows of outrageous fortune," << endl;
+	//cout << "Or to take arms against a sea of troubles,? " << endl;
+	//cout << "And by opposing end them? " << endl;
+	//cout << "William Shakespeare" << endl;
+
+	//cout << "1.\"Hello, World!\"" << endl;
+	//cout << "2.\\Bjarne Stroustrup\\" << endl;
+
+	/*cout << "1.\"Hello, World!\"" << endl;
+	cout << "2.\//Dennis Ritchi\//" << endl;*/
+
+	/*cout << "1.Gather all dirty dishes..\n";
+	cout << "2.Fill the sink with warm water and add dish soap..\n";
+	cout << "3.Place the dishes into the soapy water.\n";
+	cout << "4.Scrub each dish with a sponge or brush:.\n";
+	cout << "5.Dry the dishes with a clean towel\n";
+	cout << "6.Put the dry dishes away in their proper places.\n";*/
+
+	//cout << "1.\tInput the radius of the cylinder's base (R).>\n";
+	//cout << "2.\t\tInput the height of the cylinder (H).>>\n";
+	//cout << "3.\t\t\tCalculate the volume:>>\n";
+	//cout << "4.\t\t\t\tUse the formula: Volume = π * R^2 * H.>>\n";
+	//cout << "5.\t\t\t\t\tOutput the volume of the cylinder>>\n";
+
+	//cout << "1.Input the length of the rectangle (L)..\n";
+	//cout << "2.Input the width of the rectangle (W)..\n";
+	//cout << "3.Calculate the area:\n";
+	//cout << "4.Multiply the length (L) by the width (W).\n";
+	//cout << "5.The formula is: Area = L * W..\n";
+	//cout << "6.Output the area of ​​the rectangle..\n";
+
+	//cout << "1.get dressed.\n";
+	//cout << "2.Take your wallet.\n";
+	//cout << "3.Close the door.\n";
+	//cout << "4.Go to the store.\n";
+	//cout << "5.Go to the store..\n";
+	//cout << "6.Go to the bread department..\n";
+	//cout << "7.Choose the necessary bread..\n";
+	//cout << "8.Pay for the bread..\n";
+
+	//cout << "1.Take sports clothes.\n";
+	//cout << "2.Get dressed and take all the necessary things.\n";
+	//cout << "3.Get out of the house.\n";
+	//cout << "4.We use transport.\n";
+	//cout << "5.Register.\n";
+	//cout << "6.Perform light exercises to warm up the muscles.\n";
+	//cout << "7.Perform the planned exercises.\n";
+	//cout << "8.Take a shower, change clothes.\n";
+	//cout << "9.We use transport.\n";
+
+		/*int age = 14;
+		age = 30;
+		cout << "I am" << age << " years old " << endl;
+		float pi = 3.14;
+		cout << "The value of pi si " << pi << endl;
+		const double PI = 3.14159265359;
+		pi = 3.14;
+		char myLEtter = 'Y';
+		bool isHAppy = false;
+		cout << isHAppy << endl;*/
+}
